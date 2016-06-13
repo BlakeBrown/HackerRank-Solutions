8594a802dcb14918c3d03cf3f4fbb04b61ecc2ba
Blake Brown <blbrown@rivierapartners.com>
2016-06-12 19:24:05 -0700

    Update file structure

---
+++ Algorithms/Searching/Ice Cream Parlor.cpp
@@ -0,0 +1,24 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <map>
+using namespace std;
+
+
+int main() {
+	int T;
+	cin>>T;
+	while(T > 0) {
+		int M, N;
+		cin>>M>>N;
+		map<int, int> m;
+		int *c = new int[N];
+		for(int i = 0; i < N; i++) {
+			m.insert(pair<)
+		}
+		T--;
+	}
+    return 0;
+}
