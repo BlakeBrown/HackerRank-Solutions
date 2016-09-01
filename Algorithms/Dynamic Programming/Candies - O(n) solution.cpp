648d80b902bb326434d424f6ac23f377fdf4c746
Blake Brown <blakebrown129@gmail.com>
2016-08-31 22:23:37 -0400

    Update candies solutions for new testcases

--- Algorithms/Dynamic Programming/Candies - O(n) solution.cpp
+++ Algorithms/Dynamic Programming/Candies - O(n) solution.cpp
@@ -7,29 +7,29 @@ using namespace std;
 
 
 int main() {
-	int N;
+	long long N;
 	cin>>N;
-	int *rating = new int[N], *candies = new int[N];
-	for(int i = 0; i < N; i++) {
+	long long *rating = new long long[N], *candies = new long long[N];
+	for(long long i = 0; i < N; i++) {
 		cin>>rating[i];
 		candies[i] = 1; // everyone gets at least one candy
 	}
 	// First pass, give candies to the right
-	for(int i = 1; i < N; i++) {
+	for(long long i = 1; i < N; i++) {
 		if(rating[i] > rating[i-1]) {
 			candies[i] += candies[i-1];
 		}
 	}
 	// Second pass, give candies to the left
-	for(int i = N-1; i >= 0; i--) {
+	for(long long i = N-1; i >= 0; i--) {
 		// This time, we need to consider max{candies[i], candies[i+1]+1}
 		// since we don't know whether the ascending or descending trend is larger
 		if(rating[i] > rating[i+1] && candies[i] < candies[i+1]+1) {
 			candies[i] = candies[i+1]+1;
 		}
 	}
-	int result = 0;
-	for(int i = 0; i < N; i++) {
+	long long result = 0;
+	for(long long i = 0; i < N; i++) {
 		result += candies[i];
 	}
 	cout<<result;
--- Algorithms/Dynamic Programming/Candies - O(nlogn) solution.cpp
+++ Algorithms/Dynamic Programming/Candies - O(nlogn) solution.cpp
@@ -3,21 +3,21 @@ using namespace std;
 
 // Solution 2: Check candies in order of increasing rating, runs in O(nlogn)
 int main() {
-    int N;
-    int rating[100000];
-    int candy[100000];
-    vector<pair<int, int>> order;
+    long long N;
+    long long rating[100000];
+    long long candy[100000];
+    vector<pair<long long, long long>> order;
     cin>>N;
-    for(int i = 0; i < N; i++) {
+    for(long long i = 0; i < N; i++) {
         candy[i] = 1;
     }
-    for(int i = 0; i < N; i++) {
+    for(long long i = 0; i < N; i++) {
         cin>>rating[i];
         order.push_back({rating[i], i});
     }
     sort(order.begin(), order.end());
-    for(int i = 0; i < N; i++) {
-        int x = order[i].second;
+    for(long long i = 0; i < N; i++) {
+        long long x = order[i].second;
         if(x-1 >= 0 && rating[x-1] < rating[x]) {
             candy[x] = max(candy[x], candy[x-1]+1);
         }
@@ -26,7 +26,7 @@ int main() {
         }
     }
     long long total = 0;
-    for(int i = 0; i < N; i++) {
+    for(long long i = 0; i < N; i++) {
         total += candy[i];
     }
     cout<<total;
