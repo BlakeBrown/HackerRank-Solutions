22e9a91dd8d9595067b3fd331d2c1deaa11d0f2f
Amit Kumar <56012894+AmitAzadi@users.noreply.github.com>
2020-03-18 02:10:15 +0530

    updated circular array rotation solution

--- Algorithms/Warmup/Circular Array Rotation.cpp
+++ Algorithms/Warmup/Circular Array Rotation.cpp
@@ -1,25 +1,24 @@
-#include <cmath>
-#include <cstdio>
-#include <vector>
-#include <iostream>
-#include <algorithm>
+#include<bits/stdc++.h>
 using namespace std;
 
-// Main algorithm: Use modulo to figure out which indices we need to print without actually
-// rotating the array.
-int main() {
-	// Read input
-	int n, k, q;
-	cin>>n>>k>>q;
-	int *array = new int[n];
-	for(int i = 0; i < n; i++) {
-		cin>>array[i];
-	}
-	k %= n; // Remove the number of full array rotations from k
-	for(int i = 0; i < q; i++) {
-		int b;
-		cin>>b;
-		cout<<array[(n-k+b)%n]<<"\n"; // Calculate the new index for b
-	}
-    return 0;
+int main()
+{
+    int n,x,y;
+    cin>>n>>x>>y;
+    vector<int>a(n);
+    for(int i=0;i<n;i++)
+    cin>>a[i];
+    x=x%n;
+     while(y--)
+     {
+    int z;
+    cin>>z;
+    z-=x;
+    if(z<0)
+    z+=n;
+    cout<<a[z]<<endl;
+    }
 }
+    
+    
+    
