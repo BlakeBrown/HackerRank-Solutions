6b6b989b29d24a91e11970aa109636de4b363b53
Utkarsh Mishra <76392681+Utkarsh1504@users.noreply.github.com>
2021-01-28 11:11:05 +0530

    Update Python If-Else.py

--- Python/Python If-Else.py
+++ Python/Python If-Else.py
@@ -1,14 +1,7 @@
+//this is a short liner and easy one.
 
-
-
-if __name__ == '__main__':
-    n = int(input().strip())
-
-    if n % 2 ==0 and n > 20:
-        print("Not Weird")
-    elif n % 2 ==0 and n > 5 and n < 21:
-        print("Weird")
-    elif n % 2==0 and n > 1 and n < 6:
-        print("Not Weird")
-    elif n % 2 == 1:
-        print("Weird")
+n = int(input())
+if(n%2==1) or n in range (5,21):
+    print("Weird")
+else:
+    print("Not Weird")
