f60d07985fd5ab0a54a07e74163ad7ed77fc5d7c
Md. Joynal Alam <59138477+joynalam67598@users.noreply.github.com>
2021-08-21 19:53:23 +0600

    Data Types.cpp
    Data Types In C++

---
+++ 30 Days of Code/Day 01/Data Types.cpp
@@ -0,0 +1,9 @@
+    int a;
+    double c;
+    string s1,ss;
+    cin>>a;
+    cin>>c;
+    //ignores an end of line character 
+    cin.ignore();
+    getline(cin,s1);
+    cout<<a+i<<endl<<fixed<<setprecision(1)<<c+d<<endl<<s+s1<<endl;
