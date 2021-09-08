050aa5619764a94a3747efc2d9c585295594571a
mohab <ertosnsbot@yahoo.com>
2021-09-08 12:56:58 +0200

    added extra long factorial problem

--- Algorithms/Implementation/Non-divisible Subset.cpp
+++ Algorithms/Implementation/Non-divisible Subset.cpp
@@ -7,11 +7,11 @@
 using namespace std;
 
 // Algorithm basically boils down to cleverly applying modulo. We know that no 2 elements can be
-// divisible by k, which also means the mod(k) of no two numbers can sum to k. For example with 
-// k = 10, we can't have two numbers where first_number%k = 1 and second_number%k = 9, since those 
-// would sum to a multiple k. Thus we'll either take all of the numbers whose value mod k is 1, or 
+// divisible by k, which also means the mod(k) of no two numbers can sum to k. For example with
+// k = 10, we can't have two numbers where first_number%k = 1 and second_number%k = 9, since those
+// would sum to a multiple k. Thus we'll either take all of the numbers whose value mod k is 1, or
 // all of the numbers whose value mod k is 9, whichever is bigger. Don't forget that we can also
-// include one multiple of k itself (since we only require that the SUM of any two numbers isn't 
+// include one multiple of k itself (since we only require that the SUM of any two numbers isn't
 // divisible by k).
 
 int main() {
@@ -20,7 +20,7 @@ int main() {
     long long *modArr = new long long[k];
     memset(modArr, 0, sizeof(modArr));
     for(long long i = 0; i < n; i++) {
-        long long tmp; 
+        long long tmp;
         cin>>tmp;
         modArr[tmp%k]++;
     }
@@ -37,9 +37,9 @@ int main() {
        if(k%2 == 0 && i == k/2 && modArr[k/2] > 0) {
            result++;
        } else {
-           result += tmp;  
+           result += tmp;
        }
-        
+
     }
     cout<<result;
     return 0;
